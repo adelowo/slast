1f84e5041d82315b0b0c34b1c5fdd390c5439ebf
Lanre Adelowo <yo@lanre.wtf>
2024-04-28 00:32:41 +0100

    clean up

--- script/Pool.s.sol
+++ script/Pool.s.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.23;
 
 import {Script, console} from "forge-std/Script.sol";
+import "../src/Pool.sol";
 
-contract CounterScript is Script {
+contract PoolScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new Pool();
     }
 }
